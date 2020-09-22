28f1ae1cee2006b7a98ccf8ba1f49db499d8ac0f
Anamika2018081 <mit2018081@iiita.ac.in>
2020-09-22 11:27:53 +0000

    response entity added

--- src/main/java/com/restapi/controller/MyController.java
+++ src/main/java/com/restapi/controller/MyController.java
@@ -8,6 +8,9 @@ import com.restapi.model.Patient;
 import com.restapi.service.MyService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,8 +41,8 @@ public class MyController {
         return myService.getByPid(pid);
     }
 */
-
-    @PostMapping("/patient")
+/*
+ @PostMapping("/patient")
     public Patient newPatient(@RequestBody Patient patient){
         String pattern = "^([A-Za-z0-9])*$";
        
@@ -50,8 +53,42 @@ public class MyController {
                 System.out.println("Input format should be [A-Za-z0-9]");
                 return null;
         }
+    }*/
+    @PostMapping("/patient")
+    public ResponseEntity<Object> newPatient(@RequestBody Patient patient, Object Status){
+                String pattern = "^([A-Za-z0-9])*$";
+                /*String msg = "Input format should be [A-Za-z0-9]";
+                HttpHeaders header = new HttpHeaders();
+                header.add("desc", "New Patient Addded");*/
+       
+                if (patient.getFirstname().matches(pattern) && patient.getLastname().matches(pattern) && patient.getDob().isBefore(LocalDate.now()) ) {
+                    //System.out.println("valid");
+                     Patient p = myService.saveOrUpdate(patient);
+                     ////return new ResponseEntity<>(p, HttpStatus.CREATED);...........(working)
+                     //return new ResponseEntity<>(msg, HttpStatus.CREATED);
+                    //return ResponseEntity<patient>(msg, header, Status, HttpStatus.CREATED);
+                    //return ResponseEntity.status(HttpStatus.OK).headers(header).build();.........(working)
+                    
+		            return new ResponseEntity<Object>("New Patient is added", HttpStatus.CREATED);
+                } else {
+                    
+                  HttpStatus.valueOf(400);
+				// return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);.......(working)
+                   //return ResponseEntity<patient>("invalid", header,Status, HttpStatus.INTERNAL_SERVER_ERROR);
+                   return new ResponseEntity<Object>("Input format should be [A-Za-z0-9]", HttpStatus.INTERNAL_SERVER_ERROR.valueOf(400));
+                }
     }
 
+    /* @PostMapping("/collections")
+    public ResponseEntity<Person> createPerson(@RequestBody Person per) {
+        try {
+            Person _pp = perRepository.save(new Person(per.getFirstname(), per.getLastname(), per.getGender(), per.getDob()));
+            return new ResponseEntity<>(_pp, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+         }
+    }
+*/
 
 
      /*@PostMapping("/patient")
